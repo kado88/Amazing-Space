a3e82525b99e2ea244a4db2910a7daa8725b0760
Kayden Stevens <stevens.kayden@gmail.com>
2016-02-20 18:35:07 +1200

    JS Version #1

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-var el = function() {
-    
+var today = new Date();
+var hourNow = today.getHours();
+var greeting;
+
+if (hourNow > 18) {
+    greeting = "Good evening earthling!";
+} else if (hourNow > 12) {
+    greeting = "Good afternoon earthling!";
+} else if (hourNow > 0) {
+    greeting = "Good morning earthling!";
+} else {
+    greeting = "Welcome earthling!";
 }
+
+var elGreet = document.getElementById('greet');
+elGreet.textContent = greeting;
